dfd6a6df191bfae5b66c4bd9dd841129b6dfb3cb
Matiss Janis Aboltins <matiss@mja.lv>
2018-03-20 21:59:09 +0200

    Dynamic data loding

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,23 @@
-var prices = prices.median;
-var mappedPrices = Object.keys(prices).map(function(key) { return prices[key]; });
-var maxPrice = Math.max.apply(null, mappedPrices);
-var minPrice = Math.min.apply(null, mappedPrices);
+var prices = {};
+var map;
+
+var mappedPrices;
+var maxPrice;
+var minPrice;
+
+var slugify = function(text) {
+  // Use hash map for special characters
+  var specialChars = {"à":'a',"ķ":'k',"č":'c',"ē":'e',"ģ":'g',"ļ":'l',"ņ":'n',"š":'s',"ū":'u',"ž":'z',"ä":'a',"á":'a',"ā":'a',"â":'a',"æ":'a',"å":'a',"ë":'e',"è":'e',"é":'e', "ê":'e',"î":'i',"ī":'i',"ï":'i',"ì":'i',"í":'i',"ò":'o',"ó":'o',"ö":'o',"ô":'o',"ø":'o',"ù":'o',"ú":'u',"ü":'u',"û":'u',"ñ":'n',"ç":'c',"ß":'s',"ÿ":'y',"œ":'o',"ŕ":'r',"ś":'s',"ń":'n',"ṕ":'p',"ẃ":'w',"ǵ":'g',"ǹ":'n',"ḿ":'m',"ǘ":'u',"ẍ":'x',"ź":'z',"ḧ":'h',"·":'-',"/":'-',"_":'-',",":'-',":":'-',";":'-'};
+
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/./g,(target, index, str) => specialChars[target] || target) // Replace special characters using the hash map
+    .replace(/&/g, '-and-')         // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');             // Trim - from end of text
+};
 
 function calcHue(price) {
   var cur = price - minPrice;
@@ -41,33 +57,8 @@ function hslToHex(h, s, l) {
 
 var infoWindow;
 
-function setPolygon(map, region) {
-  var coords = regions[region].map(function(row) {
-    return { lng: row[0], lat: row[1] };
-  });
-
-  var color = calcHue(prices[region]);
-
-  var polygon = new google.maps.Polygon({
-    paths: coords,
-    strokeColor: color,
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: color,
-    fillOpacity: 0.35,
-  });
-  polygon.setMap(map);
-
-  polygon.addListener('click', function(event) {
-    infoWindow.setContent('Cena: ' + prices[region] + ' EUR');
-    infoWindow.setPosition(event.latLng);
-
-    infoWindow.open(map);
-  });
-}
-
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: {
       lat: 56.946285,
@@ -77,8 +68,47 @@ function initMap() {
   });
 
   infoWindow = new google.maps.InfoWindow;
-
-  Object.keys(regions).forEach(function (key) {
-    setPolygon(map, key);
-  });
 }
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://raw.githubusercontent.com/brokalys/data/master/data/apartment/sell-monthly.csv', true);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState == XMLHttpRequest.DONE) {
+    var parts = $.csv.toArrays(xhr.responseText);
+    var header = parts[0].map((name) => slugify(name));
+    var priceData = parts[parts.length - 2];
+
+    for (var i = 0; i < header.length; i++) {
+      prices[header[i]] = parseInt(priceData[i], 10);
+    }
+
+    mappedPrices = Object.keys(prices).map(function(key) { return prices[key]; });
+    maxPrice = Math.max.apply(null, mappedPrices);
+    minPrice = Math.min.apply(null, mappedPrices);
+
+    initMap();
+    map.data.loadGeoJson('https://raw.githubusercontent.com/brokalys/sls-data-extraction/master/data/riga-geojson.json');
+
+    map.data.setStyle(function(feature) {
+      var region = slugify(feature.getProperty('apkaime'));
+      var color = calcHue(prices[region]);
+
+      return {
+        strokeColor: color,
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: color,
+        fillOpacity: 0.35,
+      };
+    });
+
+    map.data.addListener('click', function(event) {
+      var region = slugify(event.feature.getProperty('apkaime'));
+      infoWindow.setContent('Mediānā dzīvokļa cena: ' + prices[region] + ' EUR');
+      infoWindow.setPosition(event.latLng);
+
+      infoWindow.open(map);
+    });
+  }
+};
+xhr.send();
