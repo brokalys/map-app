dc25f3f1ca08b3bc0b094faa93abe52500ab9476
Matiss Janis Aboltins <matiss@mja.lv>
2018-03-24 19:15:51 +0200

    Cleanup & moved to nice color map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Gmaps } from 'react-gmaps';
 import parse from 'csv-parse/lib/sync';
-import slugify from 'slugify';
+import colormap from 'colormap';
 
 const coords = {
   lat: 56.946285,
@@ -35,48 +35,57 @@ class App extends React.Component {
       disableDefaultUI: true
     });
 
-    const response = await fetch('https://raw.githubusercontent.com/brokalys/data/master/data/apartment/sell-monthly.csv');
-    const csvData = await response.text();
-    const data = parse(csvData);
+    const { header, body } = await this.loadPriceData();
+    const priceData = body[body.length - 1];
+    const uniquePrices = [...new Set(priceData)].length;
 
-    const header = data[0].map((name) => slugify(name));
-    const priceData = data[data.length - 2];
+    const colors = colormap({
+      colormap: 'jet',
+      nshades: uniquePrices,
+      format: 'hex',
+    });
 
-    const prices = {};
+    this.regions = header.map((name, index) => {
+      return {
+        name,
+        price: parseInt(priceData[index], 10),
+      };
+    })
+      .sort((a, b) => a.price - b.price)
+      .map((region, index,  all) => {
+        const prev = all[index - 1] || {};
+        const colorIndex = prev.price === region.price ? prev.colorIndex || 0 : prev.colorIndex + 1;
 
-    for (var i = 3; i < header.length; i++) {
-      prices[header[i]] = parseInt(priceData[i], 10);
-    }
+        region.colorIndex = colorIndex;
+        region.color = colors[colorIndex];
 
-    const mappedPrices = Object.keys(prices).map((key) => prices[key]);
-    const maxPrice = Math.max.apply(null, mappedPrices);
-    const minPrice = Math.min.apply(null, mappedPrices);
+        return region;
+      });
 
     map.data.setStyle((feature) => {
-      const region = slugify(feature.getProperty('apkaime'));
-      const price = prices[region];
-      const color = this.calcHue(price, maxPrice, minPrice);
+      const regionName = feature.getProperty('apkaime');
+      const region = this.findRegionByName(regionName);
 
-      if (price <= 0) {
+      if (region.price <= 0) {
         return;
       }
 
       return {
-        strokeColor: color,
+        strokeColor: region.color,
         strokeOpacity: 0.8,
         strokeWeight: 2,
-        fillColor: color,
-        fillOpacity: 0.2,
+        fillColor: region.color,
+        fillOpacity: 0.4,
       };
     });
 
     const infoWindow = new window.google.maps.InfoWindow;
     map.data.addListener('click', (event) => {
-      const name = event.feature.getProperty('apkaime');
-      const region = slugify(name);
-      const price = prices[region].toFixed().replace(/(\d)(?=(\d{3})+(,|$))/g, '$1\'');
+      const regionName = event.feature.getProperty('apkaime');
+      const region = this.findRegionByName(regionName);
+      const price = region.price.toFixed().replace(/(\d)(?=(\d{3})+(,|$))/g, '$1\'');
 
-      infoWindow.setContent(`Mediānā dzīvokļa pārdošanas cena: ${price} EUR (${name})`);
+      infoWindow.setContent(`Mediānā dzīvokļa pārdošanas cena: ${price} EUR (${region.name})`);
       infoWindow.setPosition(event.latLng);
       infoWindow.open(map);
     });
@@ -97,40 +106,19 @@ class App extends React.Component {
     );
   }
 
-  calcHue(price, maxPrice, minPrice) {
-    var cur = price - minPrice;
-    var max = maxPrice - minPrice;
-    var hue = Math.floor(((1 - cur / max) * 100) * 1.2);
-    return this.hslToHex(hue, 50, 50);
+  async loadPriceData() {
+    const response = await fetch('https://raw.githubusercontent.com/brokalys/data/master/data/apartment/sell-monthly.csv');
+    const csvData = await response.text();
+    const data = parse(csvData);
+
+    return {
+      header: data[0].slice(3),
+      body: data.slice(1).map((row) => row.slice(3)),
+    };
   }
 
-  hslToHex(h, s, l) {
-    h /= 360;
-    s /= 100;
-    l /= 100;
-    var r, g, b;
-    if (s === 0) {
-      r = g = b = l; // achromatic
-    } else {
-      var hue2rgb = function (p, q, t) {
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1 / 6) return p + (q - p) * 6 * t;
-        if (t < 1 / 2) return q;
-        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-        return p;
-      };
-      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      var p = 2 * l - q;
-      r = hue2rgb(p, q, h + 1 / 3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1 / 3);
-    }
-    var toHex = function(x) {
-      var hex = Math.round(x * 255).toString(16);
-      return hex.length === 1 ? '0' + hex : hex;
-    };
-    return '#' + toHex(r) + toHex(g) + toHex(b);
+  findRegionByName(name) {
+    return this.regions.find((region) => region.name === name);
   }
 
 }
