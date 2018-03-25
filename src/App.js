529a249e0c202e482754a750609bf2985736e6c6
Matiss Janis Aboltins <matiss@mja.lv>
2018-03-25 15:24:42 +0300

    Hacky version of changing type & category

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Gmaps } from 'react-gmaps';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import parse from 'csv-parse/lib/es5/sync';
 import colormap from 'colormap';
 
@@ -31,12 +32,55 @@ const params = {
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.onMapCreated = this.onMapCreated.bind(this);
+    this.toggleCategory = this.toggleCategory.bind(this);
+    this.toggleType = this.toggleType.bind(this);
+    this.state = {
+      categoryDropdownOpen: false,
+      typeDropdownOpen: false,
+      category: 'apartment',
+      type: 'sell'
+    };
+  }
+
+  toggleCategory() {
+    this.setState({
+      categoryDropdownOpen: !this.state.categoryDropdownOpen,
+    });
+  }
+
+  toggleType(gg) {
+    this.setState({
+      typeDropdownOpen: !this.state.typeDropdownOpen,
+    });
+  }
+
+  selectCategory(value) {
+    this.state.category = value;
+    this.onMapCreated(this.map);
+  }
+
+  selectType(value) {
+    this.state.type = value;
+    this.onMapCreated(this.map);
+  }
+
   async onMapCreated(map) {
-    map.data.loadGeoJson('https://raw.githubusercontent.com/brokalys/sls-data-extraction/master/data/riga-geojson.json')
+    if (this.map === undefined) {
+      map.data.loadGeoJson('https://raw.githubusercontent.com/brokalys/sls-data-extraction/master/data/riga-geojson.json')
+    }
+
+    this.map = map;
 
     map.setOptions({
       disableDefaultUI: true,
       zoomControl: true,
+      zoomControlOptions: {
+        position: window.google.maps.ControlPosition.TOP_LEFT,
+      },
     });
 
     const { header, body } = await this.loadPriceData();
@@ -88,29 +132,80 @@ class App extends React.Component {
       const region = this.findRegionByName(regionName);
       const price = region.price.toFixed().replace(/(\d)(?=(\d{3})+(,|$))/g, '$1\'');
 
-      infoWindow.setContent(`Mediānā dzīvokļa pārdošanas cena:<br>${price} EUR (${region.name})`);
+      infoWindow.setContent(`Mediānā cena:<br>${price} EUR (${region.name})`);
       infoWindow.setPosition(event.latLng);
       infoWindow.open(map);
     });
   }
 
   render() {
+    let selectedType;
+    let selectedCategory;
+
+    switch (this.state.type) {
+      case 'rent':
+        selectedType = 'Izīrē';
+        break;
+      case 'sell':
+        selectedType = 'Pārdod';
+        break;
+    }
+
+    switch (this.state.category) {
+      case 'apartment':
+        selectedCategory = 'Dzīvoklis';
+        break;
+      case 'house':
+        selectedCategory = 'Māja';
+        break;
+      case 'land':
+        selectedCategory = 'Zeme';
+        break;
+    }
+
     return (
-      <Gmaps
-        width={'100%'}
-        height={'100%'}
-        lat={coords.lat}
-        lng={coords.lng}
-        zoom={11}
-        params={params}
-        styles={styles}
-        onMapCreated={this.onMapCreated.bind(this)}>
-      </Gmaps>
+      <div className="wrapper">
+        <Gmaps
+          width={'100%'}
+          height={'100%'}
+          lat={coords.lat}
+          lng={coords.lng}
+          zoom={11}
+          params={params}
+          styles={styles}
+          onMapCreated={this.onMapCreated}>
+        </Gmaps>
+
+        <div className="buttons">
+
+          <ButtonDropdown isOpen={this.state.categoryDropdownOpen} toggle={this.toggleCategory}>
+            <DropdownToggle outline color="danger" caret>
+              { selectedCategory }
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem onClick={this.selectCategory.bind(this, 'apartment')}>Dzīvoklis</DropdownItem>
+              <DropdownItem onClick={this.selectCategory.bind(this, 'house')}>Māja</DropdownItem>
+              <DropdownItem onClick={this.selectCategory.bind(this, 'land')}>Zeme</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+
+          <ButtonDropdown isOpen={this.state.typeDropdownOpen} toggle={this.toggleType}>
+            <DropdownToggle outline color="danger" caret>
+              { selectedType }
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem key="sell" onClick={this.selectType.bind(this, 'sell')}>Pārdod</DropdownItem>
+              <DropdownItem key="rent" onClick={this.selectType.bind(this, 'rent')}>Izīrē</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+
+        </div>
+      </div>
     );
   }
 
   async loadPriceData() {
-    const response = await fetch('https://raw.githubusercontent.com/brokalys/data/master/data/apartment/sell-monthly.csv');
+    const response = await fetch(`https://raw.githubusercontent.com/brokalys/data/master/data/${this.state.category}/${this.state.type}-monthly.csv`);
     const csvData = await response.text();
     const data = parse(csvData);
 
