69f9572395f45ba74d2f20858df2c86e8c12132b
MatissJanis <matiss@mja.lv>
2020-06-06 18:41:01 +0000

    Deploying to gh-pages from  @ 0b97cdda7d0e5bb0b15f622f0257ed763b682b99 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc12b5e112d6fa553dcd0f94d451662d.js"
+  "/precache-manifest.9419119094499991f6bd4a93d0b3e9d7.js"
 );
 
 self.addEventListener('message', (event) => {
