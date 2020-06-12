ed7f5f246559d55b697d3d7ee1bb2ec5c653eef4
Matiss Janis Aboltins <matiss@mja.lv>
2020-06-12 20:22:47 +0100

    ✅ (App) make the unit test pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<App />, {
+    wrapper: RecoilRoot,
+  });
+  const linkElement = getByText(/feedback/i);
   expect(linkElement).toBeInTheDocument();
 });
