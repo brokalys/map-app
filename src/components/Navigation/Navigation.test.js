aa6480e9af68b9bc69e0de12df5f753aa31cb244
Matiss Janis Aboltins <matiss@mja.lv>
2022-01-19 18:12:18 +0000

    🤡   fix build

--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -1,17 +1,18 @@
+import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { render } from '@testing-library/react';
+
 import Navigation from './Navigation';
 
 describe('Navigation', () => {
   test('has pinger button', () => {
-    const { getByTestId } = render(<Navigation />);
-    const pingerButton = getByTestId('pinger-button');
+    render(<Navigation />);
+    const pingerButton = screen.getByTestId('pinger-button');
     expect(pingerButton).toBeInTheDocument();
   });
 
   test('has feedback button', () => {
-    const { getByText } = render(<Navigation />);
-    const linkElement = getByText(/Feedback/i);
+    render(<Navigation />);
+    const linkElement = screen.getByText(/Feedback/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
