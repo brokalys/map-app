b259db892e2c6c0500fdb4cc9f9ca43a440621de
Matiss Janis Aboltins <matiss@mja.lv>
2022-01-22 17:18:04 +0000

    ✨   allow querying with smaller zoom

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -13,7 +13,7 @@ const containerStyle = {
   height: '100%',
 };
 
-export const MIN_ZOOM_FOR_BUILDINGS = 17;
+export const MIN_ZOOM_FOR_BUILDINGS = 15;
 export const MIN_ZOOM_FOR_HIGHLIGHTED_REGION = 12;
 export const MAX_ZOOM_FOR_HIGHLIGHTED_REGION = 14;
 
