c510d0020e1365cc325222e57f1cdab367664d27
Matiss Janis Aboltins <matiss@mja.lv>
2022-02-20 16:19:33 +0000

    ♻️   remove unnecessary console.log

--- src/hooks/api/use-rental-yield.js
+++ src/hooks/api/use-rental-yield.js
@@ -15,8 +15,6 @@ export default function useRentalYield() {
     type: 'sell',
   });
 
-  console.log('loading', isRentDataLoading, isSellDataLoading);
-
   if (
     isRentDataLoading ||
     isSellDataLoading ||
