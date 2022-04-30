353c362ef35b7c36b334ef993a16bc738b7028c8
Matiss Janis Aboltins <matiss@mja.lv>
2022-04-30 16:45:02 +0100

    🔥   remove partially-working building cache solution

--- src/hooks/api/use-region-buildings.js
+++ src/hooks/api/use-region-buildings.js
@@ -1,5 +1,4 @@
 import { gql, useQuery } from '@apollo/client';
-import pointInPolygon from 'point-in-polygon';
 
 const GET_BUILDINGS = gql`
   query UseRegionBuildings($region: String!) {
@@ -12,35 +11,7 @@ const GET_BUILDINGS = gql`
   }
 `;
 
-function isInside(polygonA, polygonB) {
-  function convertToCoordinates(str) {
-    return str.split(', ').map((part) => part.split(' ').map(parseFloat));
-  }
-
-  const coordsA = convertToCoordinates(polygonA);
-  const coordsB = convertToCoordinates(polygonB);
-
-  for (let coords of coordsB) {
-    if (!pointInPolygon(coords, coordsA)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-const localCache = {
-  cache: [],
-  set(region, value) {
-    localCache.cache.push({ region, value });
-  },
-  get(region) {
-    return localCache.cache.find((row) => isInside(row.region, region))?.value;
-  },
-};
-
 export default function useRegionBuildings(region) {
-  const cachedData = localCache.get(region);
   const { loading, error, data } = useQuery(GET_BUILDINGS, {
     variables: {
       region,
@@ -48,16 +19,7 @@ export default function useRegionBuildings(region) {
     context: {
       debounceKey: 'GET_BUILDINGS',
     },
-    skip: !!cachedData,
   });
-
-  if (cachedData) {
-    return { loading: false, data: cachedData };
-  }
-
-  if (data) {
-    localCache.set(region, data?.bounds?.buildings || []);
-  }
 
   return {
     loading,
